dec4d5979bbe2b123b2cc0729a4226c71766b411
smessie <smessie@smessie.com>
2025-07-17 14:33:23 +0200

    feat: Add example queries with multiple variations

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,128 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import DarkModeToggle from '@/components/DarkModeToggle.vue'
-import QueryEditor from '@/components/QueryEditor.vue'
+import { ref } from 'vue';
+import DarkModeToggle from '@/components/DarkModeToggle.vue';
+import QueryEditor from '@/components/QueryEditor.vue';
+import queries from '@/assets/queries.json';
 
-const endpoint = ref('')
-const query = ref('')
-const results = ref([])
-const querying = ref(false)
+const endpoint = ref('');
+const query = ref('');
+const results = ref([]);
+const querying = ref(false);
+const componentKey = ref(0);
+
+const active = ref({
+    query: '',
+    variation: '',
+});
 
 let worker: Worker | undefined;
 
 const runQuery = () => {
-  results.value = [];
-  querying.value = true;
-  //TODO: const endpoints = endpoint.value.split(',').map(e => e.trim());
-  const endpoints = endpoint.value;
-  console.log('postMessage', {
-    sources: endpoints,
-    query: query.value,
-  });
-  worker?.terminate();
-  worker = new Worker(new URL("./assets/queryWorker.ts", import.meta.url), {
-    type: "module",
-  });
-  worker.onmessage = (event: MessageEvent) => {
-    if (event.data === 'done') {
-      querying.value = false;
-      return;
-    }
-    results.value.push(JSON.parse(event.data) as never);
-  };
-  worker.postMessage({
-    sources: endpoints,
-    query: query.value,
-  });
-}
+    results.value = [];
+    querying.value = true;
+    //TODO: const endpoints = endpoint.value.split(',').map(e => e.trim());
+    const endpoints = endpoint.value;
+    console.log('postMessage', {
+        sources: endpoints,
+        query: query.value,
+    });
+    worker?.terminate();
+    worker = new Worker(new URL('./assets/queryWorker.ts', import.meta.url), {
+        type: 'module',
+    });
+    worker.onmessage = (event: MessageEvent) => {
+        if (event.data === 'done') {
+            querying.value = false;
+            return;
+        }
+        results.value.push(JSON.parse(event.data) as never);
+    };
+    worker.postMessage({
+        sources: endpoints,
+        query: query.value,
+    });
+};
 
 const stopQuery = () => {
-  worker?.terminate();
-  querying.value = false;
-}
+    worker?.terminate();
+    querying.value = false;
+};
+
+const loadVariation = (queryId: string) => {
+    const queryData = queries.find(q => q.id === queryId);
+    if (queryData) {
+        if (active.value.query !== queryId) {
+            // If another query is active, make the first variation of this query active
+            active.value.query = queryData.id;
+            active.value.variation = queryData.variations[0]?.id || '';
+            query.value = queryData.variations[0]?.query || '';
+            endpoint.value = queryData.variations[0]?.endpoint || '';
+            componentKey.value += 1; // Force re-render of QueryEditor to fix content becoming visually misaligned
+        } else {
+            const currentVariation = queryData.variations.find(v => v.id === active.value.variation);
+            const nextVariation = queryData.variations.find(v => v.id === currentVariation?.next);
+            active.value.variation = nextVariation?.id || '';
+            query.value = nextVariation?.query || '';
+            endpoint.value = nextVariation?.endpoint || '';
+            componentKey.value += 1; // Force re-render of QueryEditor to fix content becoming visually misaligned
+        }
+    }
+};
 </script>
 
 <template>
-  <main>
-    <div class="container">
-      <div class="float-right">
-        <DarkModeToggle />
-      </div>
-      <h1 class="text-3xl mb-6">Timezoneless Time Literals In SPARQL Demo</h1>
+    <main>
+        <div class="container">
+            <div class="float-right">
+                <DarkModeToggle />
+            </div>
+            <h1 class="text-3xl mb-12">Timezoneless Time Literals In SPARQL Demo</h1>
 
-      <input
-        class="mb-4 w-full bg-white dark:bg-gray-900"
-        type="url"
-        v-model="endpoint"
-        placeholder="SPARQL endpoint URL"
-      />
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-4 mb-4">
+                <button v-for="query in queries" :key="query.id" @click="loadVariation(query.id)" :class="[active.query === query.id ? query.variations.find(v => v.id === active.variation)?.border : 'border-gray-300', 'mb-4 p-2 border rounded-md bg-white dark:bg-gray-900 hover:bg-gray-200 hover:dark:bg-gray-800 active:bg-gray-300 active:dark:bg-gray-700 disabled:opacity-50']">
+                    {{ query.name }}
+                </button>
+            </div>
 
-      <QueryEditor v-model="query" />
+            <input
+                class="mb-4 w-full bg-white dark:bg-gray-900"
+                type="url"
+                v-model="endpoint"
+                placeholder="SPARQL endpoint URL"
+            />
 
-      <button
-        class="mb-4 w-full p-2 border border-gray-300 rounded-md bg-white dark:bg-gray-900 hover:bg-gray-200 hover:dark:bg-gray-800 active:bg-gray-300 active:dark:bg-gray-700 disabled:opacity-50"
-        @click="runQuery" v-if="!querying"
-      >
-        Run query
-      </button>
-      <button
-        class="mb-4 w-full p-2 border border-gray-300 rounded-md bg-white dark:bg-gray-900 hover:bg-gray-200 hover:dark:bg-gray-800 active:bg-gray-300 active:dark:bg-gray-700 disabled:opacity-50"
-        @click="stopQuery" v-else
-      >
-        Stop query
-      </button>
+            <QueryEditor v-model="query" :key="componentKey" />
 
-      <div class="w-full h-96 overflow-scroll border border-gray-300 rounded-md bg-white dark:bg-gray-900">
-        <div v-if="querying" class="m-2 text-center text-sm">
-          Already found {{ results.length }} results...
+            <button
+                class="mb-4 w-full p-2 border border-gray-300 rounded-md bg-white dark:bg-gray-900 hover:bg-gray-200 hover:dark:bg-gray-800 active:bg-gray-300 active:dark:bg-gray-700 disabled:opacity-50"
+                @click="runQuery"
+                v-if="!querying"
+            >
+                Run query
+            </button>
+            <button
+                class="mb-4 w-full p-2 border border-gray-300 rounded-md bg-white dark:bg-gray-900 hover:bg-gray-200 hover:dark:bg-gray-800 active:bg-gray-300 active:dark:bg-gray-700 disabled:opacity-50"
+                @click="stopQuery"
+                v-else
+            >
+                Stop query
+            </button>
+
+            <div class="w-full h-96 overflow-scroll border border-gray-300 rounded-md bg-white dark:bg-gray-900">
+                <div v-if="querying" class="m-2 text-center text-sm">Already found {{ results.length }} results...</div>
+                <div v-else class="m-2 text-center text-sm">Found {{ results.length }} results.</div>
+                <div
+                    v-for="result in results"
+                    :key="result"
+                    class="p-2 m-2 border border-gray-300 rounded-md bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 hover:dark:bg-gray-700"
+                >
+                    <div v-for="(value, key) in result" :key="key">
+                        <p>?{{ key }}: {{ value }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div v-else class="m-2 text-center text-sm">
-          Found {{ results.length }} results.
-        </div>
-        <div v-for="result in results" :key="result" class="p-2 m-2 border border-gray-300 rounded-md bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 hover:dark:bg-gray-700">
-          <div v-for="(value, key) in result" :key="key">
-            <p>?{{ key }}: {{ value }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
+    </main>
 </template>
 
 <style scoped></style>
--- src/components/QueryEditor.vue
+++ src/components/QueryEditor.vue
@@ -1,32 +1,48 @@
 <script setup lang="ts">
-import Yasqe from '@triply/yasqe'
-import { onMounted } from 'vue'
-import '@triply/yasqe/build/yasqe.min.css'
+import Yasqe from '@triply/yasqe';
+import { onMounted, ref } from 'vue';
+import '@triply/yasqe/build/yasqe.min.css';
 
 const model = defineModel();
 
+const yasqeRef = ref<Yasqe | null>(null);
+
 onMounted(() => {
-  if (!document.getElementById('yasqe')) {
-    console.error('Yasqe element not found')
-    return
-  }
-  const yasqe = new Yasqe(document.getElementById('yasqe')!, {
-    showQueryButton: false,
-    // @ts-expect-error Must be null to disable button, but types do not allow it.
-    createShareableLink: null,
-  })
+    const el = document.getElementById('yasqe');
+    if (!el) {
+        console.error('Yasqe element not found');
+        return;
+    }
 
-  // Already set the model value to the initial value of the editor
-  model.value = yasqe.getValue();
+    const yasqe = new Yasqe(el, {
+        showQueryButton: false,
+        // @ts-expect-error Must be null to disable button, but types do not allow it.
+        createShareableLink: null,
+    });
 
-  yasqe.on('change', () => {
-    model.value = yasqe.getValue();
-  });
-})
+    yasqeRef.value = yasqe;
+
+    // Set initial value
+    yasqe.setValue(model.value || '');
+
+    // Update model when the editor changes
+    yasqe.on('change', () => {
+        model.value = yasqe.getValue();
+    });
+});
+
+// Watch for changes from parent and update Yasqe editor
+// This is no longer needed because of the force re-render fix in the parent component
+/*watch(model, (newVal) => {
+    const yasqe = yasqeRef.value;
+    if (yasqe && newVal !== yasqe.getValue()) {
+        yasqe.setValue(newVal || '');
+    }
+});*/
 </script>
 
 <template>
-  <div id="yasqe" class="mb-4 w-full"></div>
+    <div id="yasqe" class="mb-4 w-full"></div>
 </template>
 
 <style scoped></style>
